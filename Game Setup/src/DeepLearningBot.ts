9a3dbe4e3b4fe058664a78f04eeec98e72575f06
AbhiAlest <128109884+AbhiAlest@users.noreply.github.com>
2023-04-19 18:54:50 -0500

    Update DeepLearningBot.ts
    Uses JSON file now

--- Game Setup/src/DeepLearningBot.ts
+++ Game Setup/src/DeepLearningBot.ts
@@ -1,8 +1,7 @@
 import * as tf from "@tensorflow/tfjs";
 import { ChessInstance } from "chess.js";
 
-// saved model
-const model = await tf.loadLayersModel("path/to/model.json"); //not done
+const model = await tf.loadLayersModel("path/to/model.json"); //replace with json path
 
 export const predictBestMove = (chess: ChessInstance) => {
   const moves = chess.moves();
@@ -27,8 +26,48 @@ export const predictBestMove = (chess: ChessInstance) => {
   return bestMove as string;
 };
 
-const preprocessInput = (chess: ChessInstance) => {
-  const board = chess.board();
-  const input = tf.tensor(board.flat()).reshape([1, 8, 8, 1]).div(12);
-  return input;
+const preprocessInput = (
+  chess: ChessInstance
+): tf.Tensor<tf.Rank.R4> => {
+  const gameData = chessToGameData(chess);
+  const gameDataTensor = tf.tensor4d(gameData, [1, 8, 8, 13]);
+  return gameDataTensor;
+};
+
+const chessToGameData = (chess: ChessInstance): number[][][] => {
+  const gameData: number[][][] = [];
+  const ranks = chess.SQUARES;
+  for (let rankIndex = 0; rankIndex < ranks.length; rankIndex += 8) {
+    const row: number[][] = [];
+    for (let fileIndex = 0; fileIndex < 8; fileIndex++) {
+      const square = ranks[rankIndex + fileIndex];
+      const piece = chess.get(square);
+      const pieceIndex = pieceToIndex(piece);
+      row.push([pieceIndex]);
+    }
+    gameData.push(row);
+  }
+  return gameData;
+};
+
+const pieceToIndex = (piece: string | null): number => {
+  const pieceMap: { [key: string]: number } = {
+    p: 1,
+    n: 2,
+    b: 3,
+    r: 4,
+    q: 5,
+    k: 6,
+    P: 7,
+    N: 8,
+    B: 9,
+    R: 10,
+    Q: 11,
+    K: 12,
+  };
+  if (piece === null) {
+    return 0;
+  } else {
+    return pieceMap[piece];
+  }
 };
